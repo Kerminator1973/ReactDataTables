f91768bb51fd4961ec2f0dca5e964815c2660cdc
Maksim Rozhkov <kermit@mail.ru>
2025-07-11 23:17:59 +0300

    React: добавлен комментарий о React.StrictMode

--- sources/src/main.tsx
+++ sources/src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Особенное свойство StrictMode в режиме отладки кода - рендеринг осуществляется дважды, чтобы
+// увидеть различные side-effects и убедиться в "чистоте" компонентной модели
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
