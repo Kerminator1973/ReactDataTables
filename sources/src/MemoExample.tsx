0a2fad220abea92129bc54f6e0ba63d3159873b6
Maksim Rozhkov <kermit@mail.ru>
2025-07-11 23:06:55 +0300

    #22: экспериментальнaя проверка, что мемоизация работает

--- sources/src/MemoExample.tsx
+++ sources/src/MemoExample.tsx
@@ -10,21 +10,34 @@ const ACCORDION_ITEMS  : AccordionItemType[] = [
 
 const MemoExample = () => {
 
-    // Выполняется мемоизация данные Accordion. TODO: проверить эффективность
-    const memoizedAccordionItems = useMemo(() => 
-    ACCORDION_ITEMS .map((item: AccordionItemType, index: number) => (
-        <AccordionItem key={index} item={item} index={index} />
-    )), []
-    );
+    // Выполняется мемоизация дочерних элементов Accordion
+    const memoizedAccordionItems = useMemo(() => {
+        console.log("Current high-resolution tick: " + performance.now());
+        return ACCORDION_ITEMS .map((item: AccordionItemType, index: number) => (
+            <AccordionItem key={index} item={item} index={index} />
+        ))
+    }, []);
     
-  return (
-    <>
-      {/* Аккордион, иллюстрирующий использование Context API */}
-      <Accordion>
-        {memoizedAccordionItems}
-      </Accordion>
-    </>
-  )   
+    return (
+        <>
+            <Accordion>
+                {memoizedAccordionItems}
+            </Accordion>
+
+            {/* Странное дело, но количество записей об ре-рендере с мемоизацией и без - одинаковое
+            <Accordion>
+            {
+                (() => {
+                    console.log("Re-render (without memoization): " + performance.now());
+                    return ACCORDION_ITEMS.map((item: AccordionItemType, index: number) => (
+                        <AccordionItem key={index} item={item} index={index} />
+                    ))
+                })()
+            } 
+            </Accordion>   
+            */}
+        </>
+    )   
 }
 
 export default MemoExample;
