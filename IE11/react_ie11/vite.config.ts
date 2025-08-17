84cc958bd64dc953dc6988a94250c7504d6ad23b
Maksim Rozhkov <kermit@mail.ru>
2025-08-17 11:05:52 +0300

    React и IE11: успешный запуск React-приложения в IE11

--- IE11/react_ie11/vite.config.ts
+++ IE11/react_ie11/vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    legacy({
+      targets: ['ie >= 11'],
+      additionalLegacyPolyfills: [
+        'core-js/stable',
+        'regenerator-runtime/runtime'
+      ]
+    })
   ]
 })
