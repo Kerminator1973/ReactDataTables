c4348540d9e9740db9b5f9bd4332d5d6594a514b
Maksim Rozhkov <kermit@mail.ru>
2026-08-15 15:19:39 +0300

    React-SPCD: добавлен Endpoint, который возвращает изображение из папки "assets"

--- react-spcd/backend/BackendApi/Program.cs
+++ react-spcd/backend/BackendApi/Program.cs
@@ -1,7 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using BackendApi.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+
+var options = new WebApplicationOptions
+{
+    Args = args,
+    // Указываем, что wwwroot будет находится в подпапке "assets" проекта,
+    // относительно папки, в которой находится .csproj
+    WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "assets")
+};
+
+var builder = WebApplication.CreateBuilder(options);
 
 builder.Services
     .AddDbContext<AppDbContext>(options =>
@@ -20,16 +29,46 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection(); // Автоматически переводим протокол с http на https
 
-// --- Определяем Endpoints для Products ---
+// Определяем Endpoint, по которому React-приложение сможет получить графический файл.
+// Фактически, файлы находятся в папке "assets", но получать картинку можно используя
+// путь /api/files. Пример верстки:
+//      <img src="/api/files/04-bc63-7a6a714d188d.png" />
+app.MapGet("/api/files/{fileName}", async (string fileName, HttpContext httpContext) =>
+{
+    var webRootPath = httpContext.RequestServices.GetRequiredService<IWebHostEnvironment>().WebRootPath;
+    var imagePath = Path.Combine(webRootPath, fileName);
+
+    if (!File.Exists(imagePath))
+    {
+        return Results.NotFound("Image file not found.");
+    }
+    
+    var fileExtension = Path.GetExtension(fileName).ToLower();
+    var contentType = GetContentType(fileExtension);
+
+    // Используем FileStreamResult для потоковой передачи файла
+    return Results.File(imagePath, contentType, Path.GetExtension(imagePath));
+});
+
+static string GetContentType(string extension)
+{
+    return extension switch
+    {
+        ".png" => "image/png",
+        ".jpg" => "image/jpeg",
+        ".jpeg" => "image/jpeg",
+        ".gif" => "image/gif",
+        _ => "application/octet-stream"
+    };
+}
 
 app.MapGet("/api/products/{id}", async (AppDbContext db, int id) =>
 {
-    // Проектируем Product и его документы в анонимный тип или DTO
     var productDto = await db.Products
         .Where(p => p.Id == id)
         .Select(p => new {
             p.Id,
-            p.Name, // Только нужное поле
+            p.Name,
             Documents = p.Documents.Select(d => new { d.Id, d.Name }) // Проецируем только нужные поля в документах
         })
         .ToListAsync();
@@ -40,19 +79,17 @@ app.MapGet("/api/products/{id}", async (AppDbContext db, int id) =>
     return Results.Ok(productDto[0]);
 });
 
-// Инициализация и заполнения базы данных при её создании
+// Инициализация и заполнение базы данных при её создании
 using (var scopeScope = app.Services.CreateScope())
 {
     var services = scopeScope.ServiceProvider;
     try
     {
         var dbContext = services.GetRequiredService<AppDbContext>();
-        await dbContext.Database.MigrateAsync(); // Применяем миграции и сохдаём схему базы данных
+        await dbContext.Database.MigrateAsync(); // Применяем миграции и создаём схему базы данных
     }
     catch (Exception ex)
     {
-        // TODO: очень странный код - нужно с ним разобраться
-        // Correctly injecting ILogger<T> 
         var logger = services.GetService<Microsoft.Extensions.Logging.ILogger<Program>>();
         logger?.LogError(ex, "An error occurred while seeding the database.");
     }
