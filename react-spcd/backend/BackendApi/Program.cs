3da75d986707b22ecc1d74576a599ed41eff6bfc
Maksim Rozhkov <kermit@mail.ru>
2026-08-15 10:14:49 +0300

    React-SPCD: удалены избыточные запросы

--- react-spcd/backend/BackendApi/Program.cs
+++ react-spcd/backend/BackendApi/Program.cs
@@ -22,11 +22,6 @@ app.UseHttpsRedirection(); // Автоматически переводим пр
 
 // --- Определяем Endpoints для Products ---
 
-app.MapGet("/api/products", async (AppDbContext db) => 
-{
-    return await db.Products.ToListAsync();
-}).WithName("GetAllProducts");
-
 app.MapGet("/api/products/{id}", async (AppDbContext db, int id) =>
 {
     // Проектируем Product и его документы в анонимный тип или DTO
@@ -44,31 +39,6 @@ app.MapGet("/api/products/{id}", async (AppDbContext db, int id) =>
     // Возвращаем первый элемент и передаем его как DTO
     return Results.Ok(productDto[0]);
 });
-
-app.MapPost("/api/products", async (AppDbContext db, Product product) => 
-{
-    db.Products.Add(product);
-    await db.SaveChangesAsync();
-
-    var createdProduct = await db.Products.FindAsync(product.Id);
-    return Results.Created($"/api/products/{createdProduct!.Id}", createdProduct);
-}).WithName("CreateProduct");
-
-// --- Определяем Endpoints для добавления документов к продукту ---
-
-// POST - документ всегда привязан к продукту. Добавить документ можно только через продукт
-app.MapPost("/api/products/{productId}/documents", async (AppDbContext db, int productId, Document newDocument) => 
-{
-    var product = await db.Products.FindAsync(productId);
-    if (product == null) return Results.NotFound("Product not found.");
-
-    newDocument.ProductId = productId;
-    db.Documents.Add(newDocument);
-    await db.SaveChangesAsync();
-
-    var updatedProduct = await db.Products.Include(p => p.Documents).FirstOrDefaultAsync(p => p.Id == productId);
-    return Results.Created($"/api/products/{productId}", updatedProduct);
-}).WithName("CreateDocument");
 
 // Инициализация и заполнения базы данных при её создании
 using (var scopeScope = app.Services.CreateScope())
