fa42448d0fa24cba17f468a7d4c310653bbc3984
Maksim Rozhkov <kermit@mail.ru>
2026-08-29 23:07:34 +0300

    Проект React-SPCD выделен в отдельный репозитарий на GitVerse

--- react-spcd/src/components/PageFooter.tsx
+++
@@ -1,9 +0,0 @@
-const PageFooter = () => {
-  return (
-    <footer className='py-8 mt-12 border-t bg-gray-100 text-center'>
-      © {new Date().getFullYear()} Vibe Corporation. All rights reserved.
-    </footer>
-  );
-};
-
-export default PageFooter;
--- react-spcd/src/types/AnnotatedPicture.tsx
+++
@@ -1,7 +0,0 @@
-interface AnnotatedPicture {
-    id: number;
-    name: string;
-    image: string
-}
-
-export type { AnnotatedPicture };
